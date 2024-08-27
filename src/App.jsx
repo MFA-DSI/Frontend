180196f593aaf0a0120f0caa243c64f1782d4527
Jonatana RANDRIAMPARANY <103509534+JonatanaRandria@users.noreply.github.com>
2024-08-27 15:59:08 +0300

    chore: fix import

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import {Route, Routes} from "react-router-dom";
-import "./App.css";
-
 import {AuthWrapper, PrivateWrapper} from "./components/Auth";
 import Home from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
-
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 
